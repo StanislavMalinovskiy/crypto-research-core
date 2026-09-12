b873dca9a90fc0f8b085ac73705431d345612214
Stanislav Malinovskiy <stasma@hotmail.com>
2026-09-12 23:33:50 +0500

    Initial commit

---
+++ src/test/java/io/cryptoresearch/crypto_research_core/CryptoResearchCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cryptoresearch.crypto_research_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoResearchCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
